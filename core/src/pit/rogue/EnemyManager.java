c0a7c57ccbcf0a2f832ba8f557954a51287dbf00
Clemens Hartmann <clemens@Admins-MacBook-Pro.local>
2017-10-01 10:09:31 +0200

    Added Sounds and Music

--- core/src/pit/rogue/EnemyManager.java
+++ core/src/pit/rogue/EnemyManager.java
@@ -4,8 +4,15 @@ import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Sound;
+
 public class EnemyManager {
 	private static List<Enemy> enemys = new LinkedList<Enemy>();
+	
+	private Sound itemAppearsSound = Gdx.audio.newSound(Gdx.files.internal("sounds/chest appears 2.wav"));
+	
+	private Sound enemyDiesSound = Gdx.audio.newSound(Gdx.files.internal("sounds/enemy dies.wav"));
 	
 	public void update(float delta, float playerX, float playerY, Room room) {
 		Iterator<Enemy> iter = enemys.iterator();
@@ -13,11 +20,13 @@ public class EnemyManager {
 			Enemy enemy = iter.next();
 			enemy.update(delta, playerX, playerY);
 			if(!enemy.isAlive()) {
+				enemyDiesSound.play();
 				if(Math.random() > 0.7) {
 					room.addItem(enemy.getX(), enemy.getY(), 0);
 				}
 				if(EnemyManager.getEnemys().size() == 1) {
 					if(Math.random() > 0.7) {
+						itemAppearsSound.play();
 						room.addItem(Config.WIDTH/2, Config.HEIGHT/2 - 32, 1);
 					}
 				}
